4d130940c06546fc2c84fb3f1e2aed840ccfa362
Leo Pitsillides <leo.p@modux.co.uk>
2024-05-26 08:18:16 +0100

    Docker setup for dashboard. Will need to setup quick reload.

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -3,6 +3,14 @@ import solidPlugin from "vite-plugin-solid";
 import suidPlugin from "@suid/vite-plugin";
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+  },
+  preview: {
+    host: true,
+    port: 5173,
+  },
   plugins: [suidPlugin(), solidPlugin()],
   build: {
     target: "esnext",
